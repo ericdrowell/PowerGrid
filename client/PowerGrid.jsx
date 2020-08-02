80d59c7dd0f338ef6c656b8852985870c3d324e4
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2020-08-01 22:48:46 -0700

    more stuff

--- client/PowerGrid.jsx
+++ client/PowerGrid.jsx
@@ -1,23 +1,43 @@
 const React = require('react');
 
+let getStarts = (sizes) => {
+  let starts = [];
+  let start = 0;
+  sizes.forEach((size, n) => {
+    starts[n] = start;
+    start += size;
+  });
+
+  return starts;
+}
+
 module.exports = (props) => {
-  let state = props.state;
+  let viewModel = props.viewModel;
+  let colWidths = viewModel.colWidths;
+  let rowHeights = viewModel.rowHeights;
+  let colStarts = getStarts(colWidths);
+  let rowStarts = getStarts(rowHeights);
 
   // for now render all.  will need to filter down
-  let viewportCells = state.cells;
+  let viewportCells = viewModel.cells;
 
   let reactCells = [];
 
   viewportCells.forEach((cell) => {
-    let x = 0;
-    let y = 100;
+    let cellViewModel = cell.viewModel;
+    let x = colStarts[cellViewModel.col];
+    let y = rowStarts[cellViewModel.row];
+    let width = colWidths[cellViewModel.col];
+    let height = rowHeights[cellViewModel.row];
 
-    let innerCell = React.createElement(cell.renderer, cell.viewModel, []);
+    let innerCell = React.createElement(cell.renderer, cellViewModel, []);
 
     let outerCell = React.createElement('div', {
       className: 'power-grid-cell',
       style: {
-        transform: 'translate(' + x + 'px ,' +  y + 'px)'
+        transform: 'translate(' + x + 'px ,' +  y + 'px)',
+        width: width + 'px',
+        height: height + 'px'
       }
     }, [innerCell])
 
@@ -25,7 +45,11 @@ module.exports = (props) => {
   });
 
   let grid = React.createElement('div', {
-    className: 'power-grid'
+    className: 'power-grid',
+    style: {
+      width: viewModel.width + 'px',
+      height: viewModel.height + 'px'
+    }
   }, reactCells);
 
   return grid;
--- client/app.jsx
+++ client/app.jsx
@@ -6,6 +6,8 @@ const TextCell = require('./cells/TextCell/TextCell.jsx');
 let appContainer = document.querySelector('#app');
 
 let viewModel = {
+  width: 200,
+  height: 200,
   x: 100,
   y: 100,
   colWidths: [50, 50, 50, 50],
@@ -14,40 +16,40 @@ let viewModel = {
     {
       renderer: TextCell,
       viewModel: {
-        value: 'A'
-      },
-      col: 0,
-      row: 0
+        value: 'A',
+        col: 0,
+        row: 0
+      }
     },
     {
       renderer: TextCell,
       viewModel: {
-        value: 'B'
-      },
-      col: 0,
-      row: 1
+        value: 'B',
+        col: 1,
+        row: 0
+      }
     },
     {
       renderer: TextCell,
       viewModel: {
-        value: 'C'
-      },
-      col: 0,
-      row: 2
+        value: 'C',
+        col: 0,
+        row: 1
+      }
     },
     {
       renderer: TextCell,
       viewModel: {
-        value: 'D'
-      },
-      col: 0,
-      row: 3
+        value: 'D',
+        col: 1,
+        row: 1
+      }
     }
   ]
 };
 
 let render = () => {
-  ReactDom.render(<PowerGrid state={viewModel} />, appContainer);
+  ReactDom.render(<PowerGrid viewModel={viewModel} />, appContainer);
 };
 
 render();
