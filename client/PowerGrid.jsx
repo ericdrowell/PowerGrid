4ccc19dd094c11d7d45f3e2cd0e474d5a080b8a4
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2020-08-01 22:24:12 -0700

    more stuff

--- client/PowerGrid.jsx
+++ client/PowerGrid.jsx
@@ -3,9 +3,30 @@ const React = require('react');
 module.exports = (props) => {
   let state = props.state;
 
-  return (
-    <div className="power-grid">
+  // for now render all.  will need to filter down
+  let viewportCells = state.cells;
 
-    </div>
-  )
+  let reactCells = [];
+
+  viewportCells.forEach((cell) => {
+    let x = 0;
+    let y = 100;
+
+    let innerCell = React.createElement(cell.renderer, cell.viewModel, []);
+
+    let outerCell = React.createElement('div', {
+      className: 'power-grid-cell',
+      style: {
+        transform: 'translate(' + x + 'px ,' +  y + 'px)'
+      }
+    }, [innerCell])
+
+    reactCells.push(outerCell);
+  });
+
+  let grid = React.createElement('div', {
+    className: 'power-grid'
+  }, reactCells);
+
+  return grid;
 }
--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,50 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 const PowerGrid = require('./PowerGrid.jsx');
+const TextCell = require('./cells/TextCell/TextCell.jsx');
 
 let appContainer = document.querySelector('#app');
 
-let viewModel = {};
+let viewModel = {
+  x: 100,
+  y: 100,
+  colWidths: [50, 50, 50, 50],
+  rowHeights: [20, 20, 20, 20],
+  cells: [
+    {
+      renderer: TextCell,
+      viewModel: {
+        value: 'A'
+      },
+      col: 0,
+      row: 0
+    },
+    {
+      renderer: TextCell,
+      viewModel: {
+        value: 'B'
+      },
+      col: 0,
+      row: 1
+    },
+    {
+      renderer: TextCell,
+      viewModel: {
+        value: 'C'
+      },
+      col: 0,
+      row: 2
+    },
+    {
+      renderer: TextCell,
+      viewModel: {
+        value: 'D'
+      },
+      col: 0,
+      row: 3
+    }
+  ]
+};
 
 let render = () => {
   ReactDom.render(<PowerGrid state={viewModel} />, appContainer);
