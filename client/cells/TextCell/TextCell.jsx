6164e094f50aa3c7e430467b4c14d721ae808916
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2020-08-13 13:37:07 -0700

    tweaks

--- client/cells/TextCell/TextCell.jsx
+++ client/cells/TextCell/TextCell.jsx
@@ -3,8 +3,8 @@ const React = require('react');
 module.exports = (props) => {
   let viewModel = props.viewModel;
   return (
-    <div className={'power-grid-cell power-grid-text-cell ' + viewModel.rating} onClick={props.onClick} data-row={props.row} style={{transform: 'translate(' + props.x + 'px,' + props.y + 'px)', width: props.width+'px', height: props.height+'px'}}>
+    <td className={'power-grid-cell power-grid-text-cell ' + viewModel.rating} onClick={props.onClick} data-row={props.row} style={{transform: 'translate(' + props.x + 'px,' + props.y + 'px)', width: (props.width-2)+'px', height: props.height+'px'}}>
       {viewModel.value}
-    </div>
+    </td>
   )
 }
